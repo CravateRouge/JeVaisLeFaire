2fe7614c989ed14c12fb0ebc0d3a5691348a854b
DocPatate <Simon@192.168.194.209>
2016-03-09 09:21:58 +0100

    Implementation de l'IA

---
+++ src/listener/BattleListener.java
@@ -0,0 +1,9 @@
+package listener;
+
+import java.util.EventListener;
+
+public interface BattleListener extends EventListener {
+
+	void battleChoisi(String battle);
+
+}
--- src/vue/BatailleFenetre.java
+++ src/vue/BatailleFenetre.java
@@ -2,13 +2,11 @@ package vue;
 
 import java.awt.CardLayout;
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
