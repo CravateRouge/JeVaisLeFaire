ce5b57bd8b2ae323a45525ddb64c6b3cfdc04994
CravateRouge <heisenberg@walterwhite>
2016-04-06 09:45:36 +0200

    Modifs mineures
    Signed-off-by: CravateRouge <heisenberg@walterwhite>

--- src/modele/Case.java
+++ src/modele/Case.java
@@ -15,6 +15,31 @@ public class Case extends AbstractCase {
 		this.y = y;
 	}
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + x;
+		result = prime * result + y;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Case))
+			return false;
+		Case other = (Case) obj;
+		if (x != other.x)
+			return false;
+		if (y != other.y)
+			return false;
+		return true;
+	}
+
 	public int getX(){
 		return this.x;
 	}
